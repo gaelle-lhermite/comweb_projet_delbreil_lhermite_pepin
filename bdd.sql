fd15933eb0775e51df4abe8689970c0160d7281e
Gaelle L'hermite <glhermite@bordeaux-inp.fr>
2025-05-07 11:44:52 +0200

    L'HERMITE. main (v5) : modification du .sql pour l'insertion de professeurs et de notes correctes

--- bdd.sql
+++ bdd.sql
@@ -11,12 +11,11 @@ CREATE TABLE eleves (
     nom_eleve VARCHAR(30),
     prenom_eleve VARCHAR(30),
     classe_eleve VARCHAR(15),
-    age_eleve INT,
     id_utilisateur INT,
     FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs(id_utilisateur)
 );
 
-CREATE TABLE professeur (
+CREATE TABLE professeurs (
     id_prof INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nom_prof VARCHAR(30),
     prenom_prof VARCHAR(30),
@@ -36,26 +35,30 @@ CREATE TABLE notes (
     id_eleve INT,
     FOREIGN KEY (id_matiere) REFERENCES matieres(id_matiere),
     FOREIGN KEY (id_eleve) REFERENCES eleves(id_eleve), 
-    FOREIGN KEY (id_prof) REFERENCES professeur(id_prof)
+    FOREIGN KEY (id_prof) REFERENCES professeurs(id_prof)
 );
 
 -- Insertion des utilisateurs
 INSERT INTO utilisateurs (nom_utilisateur, mdp_utilisateur)
-VALUES ('alice', 'azerty123','eleve'), ('bob', 'mdpsecurise','eleve');
+VALUES ('ldelbreil', 'azerty123','eleve'), ('lopepin', 'mdpsecurise','eleve');
 
 -- Insertion des élèves
 INSERT INTO eleves (nom_eleve, prenom_eleve, classe_eleve, age_eleve, id_utilisateur)
-VALUES ('Dupont', 'Alice', '5A', 14, 1), ('Martin', 'Bob', '5B', 15, 2);
+VALUES ('Delbreil', 'Laura', '1A', 'ldelbreil'), ('Pepin', 'Lou', '1A', 'lopepin');
+
+-- Insertion des professeurs
+INSERT INTO professeurs (nom_prof, prenom_prof, id_utilisateur)
+VALUES ('Placin', 'Frédéric', 'fplacin');
 
 -- Insertion des matières
 INSERT INTO matieres (nom_matiere)
 VALUES ('Maths'), ('Français'), ('Histoire');
 
 -- Insertion des notes
-INSERT INTO notes (valeur_note, id_matiere, id_eleve)
+INSERT INTO notes (valeur_note, id_matiere, id_eleve, id_prof)
 VALUES
-  (15.50, 1, 1),
-  (17.00, 1, 1),
-  (12.00, 2, 1),
-  (13.00, 2, 2),
-  (10.00, 3, 1);
+  (15.50, 1, 'ldelbreil', 'fplacin'),
+  (17.00, 1, 'ldelbreil', 'fplacin'),
+  (12.00, 2, 'lopepin', 'fplacin'),
+  (13.00, 2, 'ldelbreil', 'fplacin'),
+  (10.00, 3, 'lopepin', 'fplacin');
